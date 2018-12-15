44481f890b62f7e0f6ed9f85fe933b8ee4048993
okoriko <eurico@phybbit.com>
2018-12-15 15:51:23 +0900

    Update goth

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule GCloudex.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.11"},
-      {:goth,      "~> 0.7.2"},
+      {:goth,      "~> 0.11"},
       {:poison,    "~> 3.0"},
       {:credo,     "~> 0.3.13", only: [:dev, :test]},
       {:ex_doc,    ">= 0.11.0", only: [:dev]},
